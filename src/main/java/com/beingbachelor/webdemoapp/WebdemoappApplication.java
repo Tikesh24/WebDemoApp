6152f5e27736565eaf750994b63930db8db0dc97
Tikesh24 <tikeshverma2494@gmail.com>
2022-03-25 00:08:11 +0530

    Initial commit

---
+++ src/main/java/com/beingbachelor/webdemoapp/WebdemoappApplication.java
@@ -0,0 +1,13 @@
+package com.beingbachelor.webdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdemoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beingbachelor/webdemoapp/WebdemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beingbachelor.webdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
